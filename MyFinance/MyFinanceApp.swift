b3f49301937ac83ccc0fea06688784e9e55992e9
rahmandhika <rahmandhika19@gmail.com>
2026-04-08 05:20:43 +0700

    Initial Commit

---
+++ MyFinance/MyFinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFinanceApp.swift
+//  MyFinance
+//
+//  Created by Rahmandhika Putra Purwadi Wicaksono on 08/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MyFinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
